1226c7057a5c888771f2e62d181a21d211df0e42
yuguan <yuguan>
2019-03-30 18:01:02 -0400

    1 [CHG] add product list page;

---
+++ Vic.SportsStore.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vic.SportsStore.Domain.Abstract;
+
+namespace Vic.SportsStore.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository repository;
+
+        public ProductController(IProductsRepository productsRepository)
+        {
+            this.repository = productsRepository;
+        }
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
