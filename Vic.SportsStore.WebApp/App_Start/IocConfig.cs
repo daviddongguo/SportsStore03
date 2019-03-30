8094c360d91f5905ebe5b5b5f4d256d721dc3dde
yuguan <yuguan>
2019-03-30 17:36:15 -0400

    1 [CHG] add ioc;

---
+++ Vic.SportsStore.WebApp/App_Start/IocConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+
+namespace Vic.SportsStore.WebApp
+{
+    public class IocConfig
+    {
+        public static void Register()
+        {
+            var builder = new ContainerBuilder();
+
+            builder
+                .RegisterControllers(AppDomain.CurrentDomain.GetAssemblies())
+                .PropertiesAutowired();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
